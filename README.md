94186db4f284000c8d19d26c6a4123816ed85375
libin <libin@apusapps.com>
2024-07-08 10:00:25 +0800

    feat: release project

---
+++ README.md
@@ -0,0 +1,25 @@
+# git release version
+
+## About
+
+generate public release version from git info. designed in accordance with semantic versioning
+
+## Usage
+
+in `vite.config.ts`
+
+```typescript
+import { getVersion } from 'git-release-version'
+import { defineConfig, UserConfig } from 'vite'
+
+export default defineConfig(async (env): Promise<UserConfig> => {
+    const version = await getVersion()
+    return {}
+})
+```
+
+## Rules
+
+1. if has git info, then generate version by git info and `package.json`
+    1. style: `${major}.${minor}.${patch}-${branch}.${hash}`, `major`、 `minor`、`patch` from `pkg.version`
+2. if don't has git info, then copy from `package.json` version.
---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import semver from 'semver'
+import SimpleGit from 'simple-git'
+import assert from 'assert'
+
+import * as pkg from '../package.json'
+
+const getGitInfo = async (): Promise<{ branch: string; hash: string } | undefined> => {
+    try {
+        const git = SimpleGit(process.cwd())
+        const { current: branch } = await git.branch()
+        const log = await git.log()
+        assert(log.latest, '应该有 git log')
+        const hash: string = log.latest.hash.slice(0, 4)
+        return { branch, hash }
+    } catch (error) {
+        return undefined
+    }
+}
+/** 生成版本号 */
+const generateVersion = async (): Promise<string> => {
+    const ver = semver.parse(pkg.version)!
+    const { major, minor, patch } = ver
+    const info = await getGitInfo()
+    if (info) {
+        const { branch, hash } = info
+        return `${major}.${minor}.${patch}-${branch}.${hash}`
+    } else {
+        return `${major}.${minor}.${patch}`
+    }
+}
+
+/** 获取版本号 */
+export const getVersion = async (): Promise<string> => {
+    return await generateVersion()
+}
